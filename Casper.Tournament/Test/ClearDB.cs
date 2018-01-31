e348f3f37085c32f36354d2281f6362965e65905
CasperF <casperfroberg@gmail.com>
2018-01-31 02:21:39 +0100

    s

--- Casper.Tournament/Test/ClearDB.cs
+++ Casper.Tournament/Test/ClearDB.cs
@@ -1,24 +1,24 @@
-﻿using Casper.Tournament.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Casper.Tournament.Data;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Casper.Tournament.Test
-{
-    public static class ClearDB
-    {
-        public static void ClearDatabase()
-        {
-            using (var context = new DatabaseContext())
-            {
-                context.Database.ExecuteSqlCommand(
-                    "DELETE FROM dbo.Players WHERE ID != -1");
-                context.SaveChanges();
-            }
-        }
-    }
-}
+//namespace Casper.Tournament.Test
+//{
+//    public static class ClearDB
+//    {
+//        public static void ClearDatabase()
+//        {
+//            using (var context = new DatabaseContext())
+//            {
+//                context.Database.ExecuteSqlCommand(
+//                    "DELETE FROM dbo.Players WHERE ID != -1");
+//                context.SaveChanges();
+//            }
+//        }
+//    }
+//}
 
 
